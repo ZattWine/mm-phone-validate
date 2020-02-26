d821b2e3c7d5d18527cffb528a21850983ec8601
Zatt Wine <tomclick.org@gmail.com>
2020-02-26 10:05:46 +0630

    add acutual RegExp for phone number pattern

--- mmphonevalidate/src/main/java/com/norm/mmphonevalidate/MmPhoneValidate.kt
+++ mmphonevalidate/src/main/java/com/norm/mmphonevalidate/MmPhoneValidate.kt
@@ -7,6 +7,11 @@ import com.norm.mmphonevalidate.operator.*
  */
 object MmPhoneValidate {
 
+    /**
+     * Get operator according to phone number.
+     *
+     * @param number Phone number to check and get operator.
+     */
     fun getOperator(number: String): Operator {
         return when {
             isMpt(number) -> DefaultOperator.MPT
@@ -18,26 +23,57 @@ object MmPhoneValidate {
         }
     }
 
+    /**
+     * Validate whether the number is MPT or not.
+     *
+     * @param mptNumber Mpt number to validate.
+     */
     fun isMpt(mptNumber: String): Boolean {
         return validateNumber(mptNumber, DefaultPattern.MPT)
     }
 
+    /**
+     * Validate whether the number is [DefaultOperator.TELENOR] or not.
+     *
+     * @param telenorNumber Telenor number to validate.
+     */
     fun isTelenor(telenorNumber: String): Boolean {
         return validateNumber(telenorNumber, DefaultPattern.TELENOR)
     }
 
+    /**
+     * Validate whether the number is [DefaultOperator.MYTEL] or not.
+     *
+     * @param mytelNumber MyTel number to validate.
+     */
     fun isMytel(mytelNumber: String): Boolean {
         return validateNumber(mytelNumber, DefaultPattern.MYTEL)
     }
 
+    /**
+     * Validate whether the number is [DefaultOperator.OOREDOO] or not.
+     *
+     * @param ooredooNumber Ooredoo number to validate.
+     */
     fun isOoredoo(ooredooNumber: String): Boolean {
         return validateNumber(ooredooNumber, DefaultPattern.OOREDOO)
     }
 
+    /**
+     * Validate whether the number is [DefaultOperator.MEC] or not.
+     *
+     * @param mecNumber Mec number to validate.
+     */
     fun isMec(mecNumber: String): Boolean {
         return validateNumber(mecNumber, DefaultPattern.MEC)
     }
 
+    /**
+     * Validate the number according to defined [Pattern].
+     *
+     * @param number Phone number to validate.
+     * @param mmPattern [Pattern] to check phone number.
+     */
     fun validateNumber(number: String, mmPattern: Pattern): Boolean {
         val regex = Regex(mmPattern.pattern)
         return regex.matches(number)
--- mmphonevalidate/src/main/java/com/norm/mmphonevalidate/utils/Constants.kt
+++ mmphonevalidate/src/main/java/com/norm/mmphonevalidate/utils/Constants.kt
@@ -1,21 +1,37 @@
 package com.norm.mmphonevalidate.utils
 
-// TODO: to change the actual RegExp
-const val PATTERN_MPT = "^((884|885|886|267|268|269|405|406|407|408|409|446|266|447|887|888|889)\\d{6}|(89)\\d{7})\$"
+/**
+ * Prefix number (eg. +959 or 09).
+ */
+const val PREFIX = "(^\\+(?:95)|0)9"
+
+/**
+ * Prebuilt pattern for MPT.
+ */
+const val PATTERN_MPT = "$PREFIX((?<three>884|885|886|267|268|269|405|406|407|408|409|446|266|447|887|888|889)\\d{6}|(?<two>89|42)\\d{7})\$"
+
+/**
+ * Prebuilt pattern for TELENOR.
+ */
+const val PATTERN_TELENOR = "$PREFIX((?<three>753|754|755|756|757|758|759|750|751|752)\\d{6}|(?<two>76|77|78|79)\\d{7})\$"
+
+/**
+ * Prebuilt pattern for MYTEL.
+ */
+const val PATTERN_MYTEL = "$PREFIX((?<three>663|664|665|666|660|661|662)\\d{6}|(?<two>69|68|67)\\d{7})\$"
+
+/**
+ * Prebuilt pattern for OOREDOO.
+ */
+const val PATTERN_OOREDOO = "$PREFIX(?<two>95|96|97|98)\\d{7}\$"
+
+/**
+ * Prebuilt pattern for MEC.
+ */
+const val PATTERN_MEC = "$PREFIX(?<three>340|346|349)\\d{6}\$"
+
 const val LOGO_MPT = "https://upload.wikimedia.org/wikipedia/en/9/96/Logo_of_MPT.svg"
-
-// TODO: to change the actual RegExp
-const val PATTERN_TELENOR = "^((753|754|755|756|757|758|759|750|751|752)\\d{6}|(76|77|78|79)\\d{7})\$"
 const val LOGO_TELENOR = "https://upload.wikimedia.org/wikipedia/commons/9/9e/Telenor_Logo.svg"
-
-// TODO: to change the actual RegExp
-const val PATTERN_MYTEL = "^((663|664|665|666|660|661|662)\\d{6}|(69|68|67)\\d{7})\$"
 const val LOGO_MYTEL = "https://mytel.com.mm/themes/vmy/images/logo3.png"
-
-// TODO: to change the actual RegExp
-const val PATTERN_OOREDOO = "^(95|96|97|98)\\d{7}\$"
 const val LOGO_OOREDOO = "https://upload.wikimedia.org/wikipedia/commons/0/0c/Ooredoo_Myanmar_Logo.png"
-
-// TODO: to change the actual RegExp
-const val PATTERN_MEC = "^(340|346|349)\\d{6}\$"
 const val LOGO_MEC = ""
